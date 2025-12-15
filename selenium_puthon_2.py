6cced04c466f962d8e4618c08db89b5400020166
lavander8 <lavander8@rambler.su>
2025-12-15 14:12:32 +0300

    Имитация нажатия клавиш: Ctrl + a, Backspace

--- selenium_puthon_2.py
+++ selenium_puthon_2.py
@@ -2,11 +2,11 @@
 # открывать браузер и производить различные дествия
 import time
 
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium_python import user_name, user_password, button_login
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
@@ -23,14 +23,20 @@ driver.get(base_url)
 driver.set_window_size(1920, 1080)
 
 # Автоматическое заполнение поля Username неверным значением
-user_name = driver.find_element(By.XPATH, "//input[@id='user_name']")
-user_name.send_keys("sleep_use")
+user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
+user_name.send_keys('visual_user')
 print("Input Login")
+time.sleep(4)
+user_name.send_keys(Keys.CONTROL + "a")  # Автоматическое нажатие комбинации клавиш: Ctrl + a
+user_name.send_keys(Keys.BACKSPACE)  # Автоматическое нажатие клавиши Backspace
 
 # Автоматическое заполнение поля Password неверным значением
-password = driver.find_element(By.XPATH, "//input[@id='password']")
-password.send_keys("secret_sauce")
+user_password = driver.find_element(By.XPATH, "//input[@id='password']")
+user_password.send_keys('secret_sauce')
 print("Input Password")
+time.sleep(4)
+user_name.send_keys(Keys.CONTROL + "a")  # Автоматическое нажатие комбинации клавиш: Ctrl + a
+user_password.send_keys(Keys.BACKSPACE)  # Автоматическое нажатие клавиши Backspace
 
 # Автоматический клик по кнопке "Login"
 button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")
