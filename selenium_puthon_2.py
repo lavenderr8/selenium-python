fda4f9fc4527f37aea583f02e12341782e8eb30d
lavander8 <lavander8@rambler.su>
2025-12-15 13:21:21 +0300

    Обновление страницы

---
+++ selenium_puthon_2.py
@@ -0,0 +1,44 @@
+# Импортируем WebDriver, чтобы с ним взаимодействовать:
+# открывать браузер и производить различные дествия
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium_python import user_name, user_password, button_login
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+
+# Базовый URL для открытия
+base_url: str = 'https://www.saucedemo.com/'
+
+# Команда get для открытия ссылки
+driver.get(base_url)
+
+# Установка размеров окна браузера
+driver.set_window_size(1920, 1080)
+
+# Автоматическое заполнение поля Username неверным значением
+user_name = driver.find_element(By.XPATH, "//input[@id='user_name']")
+user_name.send_keys("sleep_use")
+print("Input Login")
+
+# Автоматическое заполнение поля Password неверным значением
+password = driver.find_element(By.XPATH, "//input[@id='password']")
+password.send_keys("secret_sauce")
+print("Input Password")
+
+# Автоматический клик по кнопке "Login"
+button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")
+button_login.click()
+print("Click Login Button")
+
+# Фнкция задержки выполнения кода на 6 сек
+time.sleep(6)
+
+# Обновление страницы
+driver.refresh()
