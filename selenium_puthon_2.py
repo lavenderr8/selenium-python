b6ab08daa498865b465ce5fc041701e28ab4d020
lavander8 <lavander8@rambler.su>
2025-12-15 15:35:26 +0300

    Имитация нажатия клавиш: Ctrl + a, Backspace

--- selenium_puthon_2.py
+++ selenium_puthon_2.py
@@ -2,6 +2,7 @@
 # открывать браузер и производить различные дествия
 import time
 
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
@@ -26,18 +27,24 @@ driver.set_window_size(1920, 1080)
 user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
 user_name.send_keys('visual_user')
 print("Input Login")
+time.sleep(4)
+user_name.send_keys(Keys.CONTROL + "a")  # Автоматическое нажатие комбинации клавиш: Ctrl + a
+user_name.send_keys(Keys.BACKSPACE)  # Автоматическое нажатие клавиши Backspace
 
 # Автоматическое заполнение поля Password неверным значением
 user_password = driver.find_element(By.XPATH, "//input[@id='password']")
 user_password.send_keys('secret_sauce')
 print("Input Password")
+time.sleep(4)
+user_name.send_keys(Keys.CONTROL + "a")  # Автоматическое нажатие комбинации клавиш: Ctrl + a
+user_password.send_keys(Keys.BACKSPACE)  # Автоматическое нажатие клавиши Backspace
 
 # Автоматический клик по кнопке "Login"
 button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")
 button_login.click()
 print("Click Login Button")
 
-# Фнкция задержки выполнения кода на 6 сек
+# Функция задержки выполнения кода на 6 сек
 time.sleep(6)
 
 # Обновление страницы
