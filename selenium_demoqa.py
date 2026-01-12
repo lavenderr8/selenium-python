3f5ccacdccac1bbeadfe3e8c6e0b9ed8ed4e53c7
lavander8 <lavander8@rambler.su>
2026-01-12 18:32:02 +0300

    Взаимодействие с Check box

---
+++ selenium_demoqa.py
@@ -0,0 +1,37 @@
+# Импортируем WebDriver, чтобы с ним взаимодействовать:
+# открывать браузер и производить различные дествия
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(
+    options=options,
+    service=ChromeService(ChromeDriverManager().install())
+)
+
+# Базовый URL для открытия
+base_url: str = 'https://demoqa.com/checkbox'
+
+# Команда get для открытия ссылки
+driver.get(base_url)
+
+# Установка размеров окна браузера
+driver.set_window_size(1920, 1080)
+
+# Найдём чек-бокс по локатору
+check_box = driver.find_element(By.XPATH, "//span[@class='rct-checkbox']")
+
+# Выполним по нему клик
+check_box.click()
+
+# Проверим, что чек-бокс выбран и выведем сообщение об этом
+check_box.is_selected()
+print("Чек-бокс выбран")
+
+# Закрываем браузер
+time.sleep(3)
+driver.close()
