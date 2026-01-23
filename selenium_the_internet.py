b4249a04c3541b4170df1dc5b4425d841e2bfe76
lavander8 <lavander8@rambler.su>
2026-01-23 20:14:56 +0300

    Взаимодействие с предупреждениями и всплывающими уведомлениями

--- selenium_the_internet.py
+++ selenium_the_internet.py
@@ -20,25 +20,31 @@ base_url: str = 'https://the-internet.herokuapp.com/javascript_alerts'
 # Команда get для открытия ссылки
 driver.get(base_url)
 
+# Находим кнопку Click for JS Alert и кликаем по ней
 js_alert = driver.find_element(By.XPATH, "//button[@onclick='jsAlert()']")
 time.sleep(2)
 js_alert.click()
 time.sleep(2)
 print("Click for JS Alert")
+# Закрываем всплывающее окно
 driver.switch_to.alert.accept()
 
+# Находим кнопку Click for JS Confirm и кликаем по ней
 js_confirm = driver.find_element(By.XPATH, "//button[@onclick='jsConfirm()']")
 time.sleep(2)
 js_confirm.click()
 time.sleep(2)
 print("Click for JS Confirm")
+# Закрываем всплывающее окно
 driver.switch_to.alert.dismiss()
 
+# Находим кнопку Click for JS Prompt и кликаем по ней
 js_prompt = driver.find_element(By.XPATH, "//button[@onclick='jsPrompt()']")
 time.sleep(2)
 js_prompt.click()
 print("Click for JS Prompt")
 time.sleep(2)
+# Печатаем слово "Hello" и закрываем всплывающее окно
 driver.switch_to.alert.send_keys("Hello")
 driver.switch_to.alert.accept()
 
