b33b398551a35bad30600fa55e6f8449901980a3
lavander8 <lavander8@rambler.su>
2026-02-02 10:30:12 +0300

    Скачивание файла в браузере

--- selenium_upload_file_demo.py
+++ selenium_upload_file_demo.py
@@ -1,12 +1,22 @@
 # Импортируем WebDriver, чтобы с ним взаимодействовать:
 # открывать браузер и производить различные действия
+import glob
+import os
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
+# Путь к директории для скачивания файлов
+path_download = "C:\\Users\\varenka\\repositories\\selenium_python\\files_download"
+
 options = webdriver.ChromeOptions()
+
+# Указываем директорию для скачивания файлов
+prefs = {'download.default_directory': path_download}
+options.add_experimental_option('prefs', prefs)
+
 options.add_argument("--start-maximized")
 
 driver = webdriver.Chrome(
@@ -15,31 +25,36 @@ driver = webdriver.Chrome(
 )
 
 # URL для открытия
-base_url: str = 'https://www.lambdatest.com/selenium-playground/upload-file-demo'
+base_url: str = 'https://www.lambdatest.com/selenium-playground/download-file-demo'
 
 # Команда get для открытия ссылки
 driver.get(base_url)
 
-# Путь к файлу, который будем загружать
-path_upload = "C:\\Users\\varenka\\repositories\\selenium_python\\file_upload\\screenshot2025.12.15-18.33.04.png"
-
-# Название файла, после загрузки
-file_name = "screenshot2025.12.15-18.33.04.png"
-
-# Загружаем файл
-select_file_button = driver.find_element(By.XPATH, "//input[@id='file']")
+# клик по кнопке "Download File"
+download_file_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Download File')]")
 time.sleep(2)
-select_file_button.send_keys(path_upload)
+download_file_button.click()
+time.sleep(3)
+
+# Имя ожидаемого файла
+file_name: str = "LambdaTest.pdf"
+
+# Формируем путь к файлу
+file_path: str = os.path.join(path_download, file_name)
+
+# Проверка, что файл скачался
+assert os.access(file_path, os.F_OK) == True
+print("Файл в директории")
 time.sleep(2)
 
-# Получаем значение атрибута value у input
-uploaded_file_value = select_file_button.get_attribute("value")
-
-# Проверяем, что имя загруженного файла присутствует в value
-if file_name in uploaded_file_value:
-    print("Файл успешно загружен, имя файла отображается корректно")
-else:
-    print("Ошибка: имя загруженного файла не найдено")
+# Проверка, что файл не пустой
+files = glob.glob(os.path.join(path_download, "*.*"))
+for file in files:
+    a = os.path.getsize(file)
+    if a > 10:
+        print("Файл не пуст")
+    else:
+        print("Файл пуст")
 
 # Закрываем браузер
 time.sleep(3)
