13226aae754f6aca31710caac53bae2d1f066635
lavander8 <lavander8@rambler.su>
2026-02-16 23:54:58 +0300

    Создание класса и метода

--- selenium_class_and_method.py
+++ selenium_class_and_method.py
@@ -10,17 +10,25 @@ from webdriver_manager.chrome import ChromeDriverManager
 # Создаём общий класс для теста
 class SauceDemoTest:
 
-    # Метод для инициализации URL и драйвера
+    # Метод для инициализация URL и драйвера
     def __init__(self, url: str):
         self.url = url
+        self.driver = self._init_driver()  # Инициализация драйвера через статический метод
 
+    # Статический метод для создания и настройки драйвера
+    @staticmethod
+    def _init_driver() -> webdriver.Chrome:
+        # Настраиваем опции браузера
         options = webdriver.ChromeOptions()
         options.add_argument("--start-maximized")
 
-        self.driver: webdriver.Chrome = webdriver.Chrome(
+        # Создаём экземпляр драйвера
+        driver = webdriver.Chrome(
             service=ChromeService(ChromeDriverManager().install()),
             options=options
         )
+
+        return driver
 
     # Метод для открытия сайта по переданному URL
     def open_site(self):
